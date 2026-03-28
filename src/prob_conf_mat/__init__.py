87a438597ccc23aed082c87f98903302a5484266
ioverho <ioverho@mailbox.org>
2026-03-28 12:33:33 +0100

    made version dynamic

--- src/prob_conf_mat/__init__.py
+++ src/prob_conf_mat/__init__.py
@@ -7,3 +7,11 @@ import scipy.stats
 
 # Import the actual Study object
 from .study import Study
+
+# Expose dynamic version
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"  # Fallback for development mode
